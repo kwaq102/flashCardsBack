601e00d420066c13c9e9543421667407d5ea3b06
Kamil Gajewski <kwaq102@gmail.com>
2023-04-14 11:27:31 +0200

    added validation to register user

--- records/register.record.ts
+++ records/register.record.ts
@@ -2,6 +2,7 @@ import { FieldPacket } from "mysql2";
 import { UserEntity } from "../types/user/user-entity";
 import { pool } from "../utils/db";
 import { v4 as uuid } from 'uuid'
+import { ValidationError } from "../utils/errors";
 
 type RegisterRecordResult = [RegisterRecord[], FieldPacket[]]
 
@@ -13,14 +14,11 @@ export class RegisterRecord implements UserEntity {
     password: string;
 
     constructor(userObj: RegisterRecord) {
-        // TODO walidacja!
-
         this.id = userObj.id;
         this.name = userObj.name;
         this.email = userObj.email;
         this.password = userObj.password;
     }
-
 
     async insertUser(): Promise<void> {
         const id = uuid();
--- routers/register.router.ts
+++ routers/register.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { RegisterRecord } from "../records/register.record";
+import { ValidationError } from "../utils/errors";
 
 export const registerRouter = Router()
     .post('/', async (req, res) => {
@@ -7,13 +8,31 @@ export const registerRouter = Router()
             ...req.body,
         };
 
+        const allUsers = await RegisterRecord.listAllUsers();
         const newUser = new RegisterRecord(data);
+
+        if (!newUser.name || newUser.name.length < 3 || newUser.name.length > 99) {
+            throw new ValidationError('Nazwa użytkownika powinna skłądać się z co najmniej 3 znaków i maksymalnie z 99.')
+        }
+        if (
+            newUser.email.length < 5 ||
+            !newUser.email.includes("@") ||
+            !newUser.email.includes(".")
+        ) {
+            throw new ValidationError('Nieprawidłowy email');
+        }
+        if (allUsers.some(user => user.email === newUser.email)) {
+            throw new ValidationError("Email już istnieje!")
+        }
+
+        if (newUser.password.length < 5) {
+            throw new ValidationError('Hasło jest zbyk krókie.');
+        }
+
         await newUser.insertUser();
         res.json(newUser);
     })
     .get('/users', async (req, res) => {
-
         const userList = await RegisterRecord.listAllUsers();
-        console.log(userList)
         res.json({ userList })
     })
